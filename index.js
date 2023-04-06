6bd78297db3883f0e406b9e20ad5d52e5cfd19c9
Isaac <isaac.ortega@alaskaair.com>
2023-04-06 14:03:40 -0700

    perf(extend): allow for custom component registration + extension

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-export * from './dist/auro-header';
+import { AuroHeader } from './src/auro-header';
+
+/**
+ * Register Custom Element.
+ * @param {Object} name - Name to use for custom element.
+ * @returns {void}
+ */
+ const registerComponent = (name = 'custom-header') => {
+  // alias definition
+  if (!customElements.get(name)) {
+    customElements.define(name, class extends AuroHeader {});
+  }
+}
+
+export { registerComponent }
