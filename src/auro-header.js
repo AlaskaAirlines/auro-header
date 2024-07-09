693c70c0a7f5704574afe41eac92a951e288cfea
Jordan Jones <jmjones2463@gmail.com>
2024-07-09 15:54:45 -0700

    perf: update eslint config and dependencies

--- src/auro-header.js
+++ src/auro-header.js
@@ -10,7 +10,7 @@ import { ifDefined } from 'lit/directives/if-defined.js';
 import styleCss from "./style-css.js";
 
 /**
- * auro-header is a custom element to make using headers with the Auro Design System seamless and easy.
+ * The auro-header component is a custom element to make using headers with the Auro Design System seamless and easy.
  *
  * @attr {String} level - Determines heading level for HTML element. Options are `1` - `6`
  * @attr {String} display - Determines presentation of header. Options are `display`, `800`, `700`, `600`, `500`, `400`, `300`.
@@ -28,15 +28,17 @@ export class AuroHeader extends LitElement {
   constructor() {
     super();
 
-    this.display = 'display'
+    this.display = 'display';
   }
 
   // function to define props used within the scope of this component
   static get properties() {
     return {
       level:    { type: String },
-      display:  { type: String,
-                  reflect: true },
+      display:  {
+        type: String,
+        reflect: true
+      },
       color:    { type: String },
       margin:   { type: String },
       size:     { type: String }
@@ -44,81 +46,84 @@ export class AuroHeader extends LitElement {
   }
 
   static get styles() {
-    return [styleCss]
+    return [styleCss];
   }
 
   /**
-   * @private Determines if the spacing is to be applied uniform or individual
-   * @param {string} size - Accepts string for size definition
-   * @returns {string} - Returns either selectors or function
+   * Determines if the spacing is to be applied uniform or individual.
+   * @private
+   * @param {string} size - Accepts string for size definition.
+   * @returns {string} - Returns either selectors or function.
    */
   spacingDecision(size) {
     if (this.margin === 'both') {
       switch (size) {
-        case 'none': return `util_stackMarginNone--top util_stackMarginNone--bottom`
-        case 'xxxs': return `util_stackMarginXxxs--top util_stackMarginXxxs--bottom`
-        case 'xxs': return `util_stackMarginXxs--top util_stackMarginXxs--bottom`
-        case 'xs': return `util_stackMarginXs--top util_stackMarginXs--bottom`
-        case 'sm': return `util_stackMarginSm--top util_stackMarginSm--bottom`
-        case 'md': return `util_stackMarginMd--top util_stackMarginMd--bottom`
-        case 'lg': return `util_stackMarginLg--top util_stackMarginLg--bottom`
-        case 'xl': return `util_stackMarginXl--top util_stackMarginXl--bottom`
-        case 'xxl': return `util_stackMarginXxl--top util_stackMarginXxl--bottom`
-        case 'xxxl': return `util_stackMarginXxl--top util_stackMarginXxl--bottom`
-        case '25': return `util_stackMargin25--top util_stackMargin25--bottom`
-        case '50': return `util_stackMargin50--top util_stackMargin50--bottom`
-        case '100': return `util_stackMargin100--top util_stackMargin100--bottom`
-        case '150': return `util_stackMargin150--top util_stackMargin150--bottom`
-        case '200': return `util_stackMargin200--top util_stackMargin200--bottom`
-        case '300': return `util_stackMargin300--top util_stackMargin300--bottom`
-        case '400': return `util_stackMargin400--top util_stackMargin400--bottom`
-        case '600': return `util_stackMargin600--top util_stackMargin600--bottom`
-        case '800': return `util_stackMargin800--top util_stackMargin800--bottom`
-        default: return ''
+        case 'none': return `util_stackMarginNone--top util_stackMarginNone--bottom`;
+        case 'xxxs': return `util_stackMarginXxxs--top util_stackMarginXxxs--bottom`;
+        case 'xxs': return `util_stackMarginXxs--top util_stackMarginXxs--bottom`;
+        case 'xs': return `util_stackMarginXs--top util_stackMarginXs--bottom`;
+        case 'sm': return `util_stackMarginSm--top util_stackMarginSm--bottom`;
+        case 'md': return `util_stackMarginMd--top util_stackMarginMd--bottom`;
+        case 'lg': return `util_stackMarginLg--top util_stackMarginLg--bottom`;
+        case 'xl': return `util_stackMarginXl--top util_stackMarginXl--bottom`;
+        case 'xxl': return `util_stackMarginXxl--top util_stackMarginXxl--bottom`;
+        case 'xxxl': return `util_stackMarginXxl--top util_stackMarginXxl--bottom`;
+        case '25': return `util_stackMargin25--top util_stackMargin25--bottom`;
+        case '50': return `util_stackMargin50--top util_stackMargin50--bottom`;
+        case '100': return `util_stackMargin100--top util_stackMargin100--bottom`;
+        case '150': return `util_stackMargin150--top util_stackMargin150--bottom`;
+        case '200': return `util_stackMargin200--top util_stackMargin200--bottom`;
+        case '300': return `util_stackMargin300--top util_stackMargin300--bottom`;
+        case '400': return `util_stackMargin400--top util_stackMargin400--bottom`;
+        case '600': return `util_stackMargin600--top util_stackMargin600--bottom`;
+        case '800': return `util_stackMargin800--top util_stackMargin800--bottom`;
+        default: return '';
       }
     } else {
-      return this.spacingApplied(size)
+      return this.spacingApplied(size);
     }
   }
 
   /**
-   * @private If spacing is individual, return will be single selector based on input
-   * @param {string} size - Accepts string for size definition
-   * @returns {string} - Returns selector
+   * If spacing is individual, return will be single selector based on input.
+   * @private
+   * @param {string} size - Accepts string for size definition.
+   * @returns {string} - Returns selector.
    */
   spacingApplied(size) {
     switch (size) {
-      case 'none': return `util_stackMarginNone--${this.margin}`
-      case '25': return `util_stackMargin25--${this.margin}`
-      case '50': return `util_stackMargin50--${this.margin}`
-      case '100': return `util_stackMargin100--${this.margin}`
-      case '150': return `util_stackMargin150--${this.margin}`
-      case '200': return `util_stackMargin200--${this.margin}`
-      case '300': return `util_stackMargin300--${this.margin}`
-      case '400': return `util_stackMargin400--${this.margin}`
-      case '600': return `util_stackMarginX600--${this.margin}`
-      case '800': return `util_stackMargin800--${this.margin}`
-      case 'xxxs': return `util_stackMarginXxxs--${this.margin}`
-      case 'xxs': return `util_stackMarginXxs--${this.margin}`
-      case 'xs': return `util_stackMarginXs--${this.margin}`
-      case 'sm': return `util_stackMarginSm--${this.margin}`
-      case 'md': return `util_stackMarginMd--${this.margin}`
-      case 'lg': return `util_stackMarginLg--${this.margin}`
-      case 'xl': return `util_stackMarginXl--${this.margin}`
-      case 'xxl': return `util_stackMarginXxl--${this.margin}`
-      case 'xxxl': return `util_stackMarginXxl--${this.margin}`
-      default: return ''
+      case 'none': return `util_stackMarginNone--${this.margin}`;
+      case '25': return `util_stackMargin25--${this.margin}`;
+      case '50': return `util_stackMargin50--${this.margin}`;
+      case '100': return `util_stackMargin100--${this.margin}`;
+      case '150': return `util_stackMargin150--${this.margin}`;
+      case '200': return `util_stackMargin200--${this.margin}`;
+      case '300': return `util_stackMargin300--${this.margin}`;
+      case '400': return `util_stackMargin400--${this.margin}`;
+      case '600': return `util_stackMarginX600--${this.margin}`;
+      case '800': return `util_stackMargin800--${this.margin}`;
+      case 'xxxs': return `util_stackMarginXxxs--${this.margin}`;
+      case 'xxs': return `util_stackMarginXxs--${this.margin}`;
+      case 'xs': return `util_stackMarginXs--${this.margin}`;
+      case 'sm': return `util_stackMarginSm--${this.margin}`;
+      case 'md': return `util_stackMarginMd--${this.margin}`;
+      case 'lg': return `util_stackMarginLg--${this.margin}`;
+      case 'xl': return `util_stackMarginXl--${this.margin}`;
+      case 'xxl': return `util_stackMarginXxl--${this.margin}`;
+      case 'xxxl': return `util_stackMarginXxl--${this.margin}`;
+      default: return '';
     }
   }
 
   /**
-   * @private Creates HTML template for use in render function
-   * @param {string} level - Accepts string value
-   * @returns {string} - Returns HTML template
+   * Creates HTML template for use in render function.
+   * @private
+   * @param {string} level - Accepts string value.
+   * @returns {string} - Returns HTML template.
    */
   template(level) {
     if (this.display === '' || this.display === null) {
-      this.display = 'display'
+      this.display = 'display';
     }
 
     switch (level) {
